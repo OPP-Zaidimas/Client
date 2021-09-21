c97b20214d92c29f02bf34595c99069a843f8718
wizacass <visak.pet0@gmail.com>
2021-09-21 19:07:36 +0300

    Create Main Menu

---
+++ Game/Views/User Controls/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Game.Views.User_Controls
+{
+    public partial class MainMenu : UserControl
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e) { }
+
+        private void FindGameButton_Click(object sender, EventArgs e) { }
+
+        private void ExitGameButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
