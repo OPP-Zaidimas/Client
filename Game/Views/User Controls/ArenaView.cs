d7bceb28732153354eeedc4c977832e0a860c526
wizacass <visak.pet0@gmail.com>
2021-10-11 19:30:59 +0300

    Resolve Merge conflict

--- Game/Views/User Controls/ArenaView.cs
+++ Game/Views/User Controls/ArenaView.cs
@@ -6,27 +6,7 @@ namespace Game.Views.User_Controls
 {
     public partial class ArenaView : UserControl
     {
-<<<<<<< HEAD
         private readonly Deck _deck;
-
-        private string HeroName
-        {
-            set
-            {
-                if (PlayerArenaSide.IsHandleCreated) PlayerArenaSide.updateLabel(value);
-            }
-        }
-
-        private string OpponentName
-        {
-            set
-            {   
-                if (EnemyArenaSide.IsHandleCreated) EnemyArenaSide.updateLabel(value);
-            }
-        }
-=======
-        private readonly Deck _deck = new();
->>>>>>> 42f87c111ddf85379ae22a68f0b643205a9d7f59
 
         public ArenaView()
         {
@@ -39,20 +19,17 @@ namespace Game.Views.User_Controls
 
             PlayerArenaSide.CreateControl();
             EnemyArenaSide.CreateControl();
-<<<<<<< HEAD
-            HeroName = heroName;
-            OpponentName = opponentName;
+
             _deck = new Deck();
-=======
+
 
             SetUsername(PlayerArenaSide, playerName);
             SetUsername(EnemyArenaSide, enemyName);
         }
 
-        private void SetUsername(ArenaSide arenaSide, string username)
+        private static void SetUsername(ArenaSide arenaSide, string username)
         {
             if (arenaSide.IsHandleCreated) arenaSide.Username = username;
->>>>>>> 42f87c111ddf85379ae22a68f0b643205a9d7f59
         }
 
         private void DrawCardButton_Click(object sender, EventArgs e)
