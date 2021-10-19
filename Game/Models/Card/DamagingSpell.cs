60651ca0c260cdba43f9c680c72870e369072a1a
arlauskaslukas <arlauskaslukas@gmail.com>
2021-10-19 11:20:17 +0300

    before rebasing

--- Game/Models/Card/DamagingSpell.cs
+++ Game/Models/Card/DamagingSpell.cs
@@ -1,10 +1,17 @@
 ﻿namespace Game.Models.Card
 {
-    public class DamagingSpell : ISpellCard
+    public class DamagingSpell : ISpellCard, IEffect
     {
         public string Title => "Spell";
         public string Description => "Damages";
         public int? Attack => null;
         public int? Defense => null;
+
+        public int EffectStrength { get => EffectStrength; set => EffectStrength = value; }
+
+        public (int, int) ApplyEffect((int index, int hp) card_data)
+        {
+            return (card_data.index, EffectStrength < card_data.hp ? card_data.hp - EffectStrength : 0);
+        }
     }
 }
--- Game/Models/Card/HealingSpell.cs
+++ Game/Models/Card/HealingSpell.cs
@@ -1,10 +1,17 @@
 ﻿namespace Game.Models.Card
 {
-    public class HealingSpell : ISpellCard
+    public class HealingSpell : ISpellCard, IEffect
     {
         public string Title => "Spell";
         public string Description => "Heals";
         public int? Attack => null;
         public int? Defense => null;
+
+        public int EffectStrength { get => EffectStrength; set => EffectStrength = value; }
+
+        public (int, int) ApplyEffect((int index, int hp) card_data)
+        {
+            return (card_data.index, card_data.hp+EffectStrength);
+        }
     }
 }
