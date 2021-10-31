1775a159e15b7fe7532c542e35531c923c57ccfb
wizacass <visak.pet0@gmail.com>
2021-10-31 18:11:45 +0200

    Implement logic to select a card

--- Game/Views/User Controls/ArenaSide.Designer.cs
+++ Game/Views/User Controls/ArenaSide.Designer.cs
@@ -35,6 +35,11 @@ namespace Game.Views.User_Controls
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.UsernameLabel = new System.Windows.Forms.Label();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.SelectButton1 = new System.Windows.Forms.Button();
+            this.SelectButton2 = new System.Windows.Forms.Button();
+            this.SelectButton3 = new System.Windows.Forms.Button();
+            this.SelectButton4 = new System.Windows.Forms.Button();
+            this.SelectButton5 = new System.Windows.Forms.Button();
             this.CardView1 = new Game.Views.User_Controls.CardView();
             this.CardView2 = new Game.Views.User_Controls.CardView();
             this.CardView3 = new Game.Views.User_Controls.CardView();
@@ -125,6 +130,11 @@ namespace Game.Views.User_Controls
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.SelectButton1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.SelectButton2, 1, 1);
+            this.tableLayoutPanel3.Controls.Add(this.SelectButton3, 2, 1);
+            this.tableLayoutPanel3.Controls.Add(this.SelectButton4, 3, 1);
+            this.tableLayoutPanel3.Controls.Add(this.SelectButton5, 4, 1);
             this.tableLayoutPanel3.Controls.Add(this.CardView1, 0, 0);
             this.tableLayoutPanel3.Controls.Add(this.CardView2, 1, 0);
             this.tableLayoutPanel3.Controls.Add(this.CardView3, 2, 0);
@@ -135,60 +145,125 @@ namespace Game.Views.User_Controls
             this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.tableLayoutPanel3.Name = "tableLayoutPanel3";
             this.tableLayoutPanel3.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowCount = 2;
             this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 157F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
             this.tableLayoutPanel3.Size = new System.Drawing.Size(684, 164);
             this.tableLayoutPanel3.TabIndex = 2;
+            // 
+            // SelectButton1
+            // 
+            this.SelectButton1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectButton1.Enabled = false;
+            this.SelectButton1.Location = new System.Drawing.Point(7, 138);
+            this.SelectButton1.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton1.Name = "SelectButton1";
+            this.SelectButton1.Size = new System.Drawing.Size(130, 20);
+            this.SelectButton1.TabIndex = 5;
+            this.SelectButton1.Text = "Select";
+            this.SelectButton1.UseVisualStyleBackColor = true;
+            this.SelectButton1.Click += new System.EventHandler(this.SelectButton1_Click);
+            // 
+            // SelectButton2
+            // 
+            this.SelectButton2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectButton2.Enabled = false;
+            this.SelectButton2.Location = new System.Drawing.Point(142, 138);
+            this.SelectButton2.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton2.Name = "SelectButton2";
+            this.SelectButton2.Size = new System.Drawing.Size(130, 20);
+            this.SelectButton2.TabIndex = 6;
+            this.SelectButton2.Text = "Select";
+            this.SelectButton2.UseVisualStyleBackColor = true;
+            this.SelectButton2.Click += new System.EventHandler(this.SelectButton2_Click);
+            // 
+            // SelectButton3
+            // 
+            this.SelectButton3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectButton3.Enabled = false;
+            this.SelectButton3.Location = new System.Drawing.Point(277, 138);
+            this.SelectButton3.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton3.Name = "SelectButton3";
+            this.SelectButton3.Size = new System.Drawing.Size(130, 20);
+            this.SelectButton3.TabIndex = 7;
+            this.SelectButton3.Text = "Select";
+            this.SelectButton3.UseVisualStyleBackColor = true;
+            this.SelectButton3.Click += new System.EventHandler(this.SelectButton3_Click);
+            // 
+            // SelectButton4
+            // 
+            this.SelectButton4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectButton4.Enabled = false;
+            this.SelectButton4.Location = new System.Drawing.Point(412, 138);
+            this.SelectButton4.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton4.Name = "SelectButton4";
+            this.SelectButton4.Size = new System.Drawing.Size(130, 20);
+            this.SelectButton4.TabIndex = 8;
+            this.SelectButton4.Text = "Select";
+            this.SelectButton4.UseVisualStyleBackColor = true;
+            this.SelectButton4.Click += new System.EventHandler(this.SelectButton4_Click);
+            // 
+            // SelectButton5
+            // 
+            this.SelectButton5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SelectButton5.Enabled = false;
+            this.SelectButton5.Location = new System.Drawing.Point(547, 138);
+            this.SelectButton5.Margin = new System.Windows.Forms.Padding(2);
+            this.SelectButton5.Name = "SelectButton5";
+            this.SelectButton5.Size = new System.Drawing.Size(130, 20);
+            this.SelectButton5.TabIndex = 9;
+            this.SelectButton5.Text = "Select";
+            this.SelectButton5.UseVisualStyleBackColor = true;
+            this.SelectButton5.Click += new System.EventHandler(this.SelectButton5_Click);
             // 
             // CardView1
             // 
             this.CardView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CardView1.Location = new System.Drawing.Point(7, 6);
+            this.CardView1.Location = new System.Drawing.Point(5, 4);
             this.CardView1.Margin = new System.Windows.Forms.Padding(0);
             this.CardView1.Name = "CardView1";
-            this.CardView1.Size = new System.Drawing.Size(130, 152);
-            this.CardView1.TabIndex = 0;
+            this.CardView1.Size = new System.Drawing.Size(134, 131);
+            this.CardView1.TabIndex = 10;
             this.CardView1.ViewModel = null;
             // 
             // CardView2
             // 
             this.CardView2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CardView2.Location = new System.Drawing.Point(142, 6);
+            this.CardView2.Location = new System.Drawing.Point(140, 4);
             this.CardView2.Margin = new System.Windows.Forms.Padding(0);
             this.CardView2.Name = "CardView2";
-            this.CardView2.Size = new System.Drawing.Size(130, 152);
-            this.CardView2.TabIndex = 1;
+            this.CardView2.Size = new System.Drawing.Size(134, 131);
+            this.CardView2.TabIndex = 11;
             this.CardView2.ViewModel = null;
             // 
             // CardView3
             // 
             this.CardView3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CardView3.Location = new System.Drawing.Point(277, 6);
+            this.CardView3.Location = new System.Drawing.Point(275, 4);
             this.CardView3.Margin = new System.Windows.Forms.Padding(0);
             this.CardView3.Name = "CardView3";
-            this.CardView3.Size = new System.Drawing.Size(130, 152);
-            this.CardView3.TabIndex = 2;
+            this.CardView3.Size = new System.Drawing.Size(134, 131);
+            this.CardView3.TabIndex = 12;
             this.CardView3.ViewModel = null;
             // 
             // CardView4
             // 
             this.CardView4.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CardView4.Location = new System.Drawing.Point(412, 6);
+            this.CardView4.Location = new System.Drawing.Point(410, 4);
             this.CardView4.Margin = new System.Windows.Forms.Padding(0);
             this.CardView4.Name = "CardView4";
-            this.CardView4.Size = new System.Drawing.Size(130, 152);
-            this.CardView4.TabIndex = 3;
+            this.CardView4.Size = new System.Drawing.Size(134, 131);
+            this.CardView4.TabIndex = 13;
             this.CardView4.ViewModel = null;
             // 
             // CardView5
             // 
             this.CardView5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.CardView5.Location = new System.Drawing.Point(547, 6);
+            this.CardView5.Location = new System.Drawing.Point(545, 4);
             this.CardView5.Margin = new System.Windows.Forms.Padding(0);
             this.CardView5.Name = "CardView5";
-            this.CardView5.Size = new System.Drawing.Size(130, 152);
-            this.CardView5.TabIndex = 4;
+            this.CardView5.Size = new System.Drawing.Size(134, 131);
+            this.CardView5.TabIndex = 14;
             this.CardView5.ViewModel = null;
             // 
             // ArenaSide
@@ -223,5 +298,15 @@ namespace Game.Views.User_Controls
         private CardView CardView3;
         private CardView CardView4;
         private CardView CardView5;
+        private System.Windows.Forms.Button SelectButton1;
+        private System.Windows.Forms.Button SelectButton2;
+        private System.Windows.Forms.Button SelectButton3;
+        private System.Windows.Forms.Button SelectButton4;
+        private System.Windows.Forms.Button SelectButton5;
+        private CardView cardView6;
+        private CardView cardView7;
+        private CardView cardView8;
+        private CardView cardView9;
+        private CardView cardView10;
     }
 }
