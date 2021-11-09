873a0b3186503259a3957f1294debf0d68f06053
Justinas <65821728+derhoras@users.noreply.github.com>
2021-11-09 15:47:46 +0200

    create interface and split classes

---
+++ Game/Services/SignalR/IService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Services.SignalR
+{
+    interface IService
+    {
+
+
+        public async Task Connect()
+        {
+        }
+
+        public async Task CreateNewGame(string username)
+        {
+        }
+
+        public async Task JoinGame(int matchId, string username)
+        {
+        }
+
+        public async Task PlaceCard((int cardId, int cardHp) card)
+        {
+        }
+
+        public async Task MonsterAttack(int attackerId, int attackerOffense, int deffenderId, int deffenderHp)
+        {
+        }
+
+        public async Task EndTurn()
+        {
+        }
+
+        public void RegisterMatchStats(MatchStats stats)
+        {
+        }
+    }
+}
