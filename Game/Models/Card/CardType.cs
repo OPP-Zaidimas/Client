beec6c5537a69006a4f2fd5d341df93dbc5c6ab4
Lukas Arlauskas <arlauskaslukas@gmail.com>
2021-12-04 08:31:22 -0800

    Started

---
+++ Game/Models/Card/CardType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Models.Card
+{
+    public enum CardType : ushort
+    {
+        Monster = 1,
+        DamagingSpellCard = 2,
+        HealingSpellCard = 3
+    }
+}
