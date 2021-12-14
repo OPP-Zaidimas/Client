eba90ee3b8892a4868c007db59a90c6bb682df9d
Justinas <65821728+derhoras@users.noreply.github.com>
2021-12-14 12:36:16 +0200

    Add stacks of multiple effects

---
+++ Game/Models/Card/Effect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Models.Card
+{
+    class Effect
+    {
+        public int EffectStrength { get; set; }
+
+        public (int, int) ApplyEffect((int index, int hp) card_data)
+        {
+            return (card_data.index, EffectStrength < card_data.hp ? card_data.hp - EffectStrength : 0);
+        }
+    }
+}
